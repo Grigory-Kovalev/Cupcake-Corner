4e8425ca3aae948f76ca969db830ebb6b4f0d1a7
Grigory Kovalev <kovalevgrigorij02@gmail.com>
2023-03-12 15:47:46 +0700

    Initial Commit

---
+++ 10.Cupcake Corner/_0_Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_Cupcake_CornerApp.swift
+//  10.Cupcake Corner
+//
+//  Created by Григорий Ковалев on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct _0_Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
